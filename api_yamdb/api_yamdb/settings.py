5795aabe81c6fb72e39a1d83a684199e2aee0167
dexie <dexie7@yande.ru>
2022-10-29 12:50:56 +0500

    My 10 commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,8 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY'),
 
 DEBUG = os.environ.get('DEBUG', False)
 
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS'),
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', default='*'),
 
 
 INSTALLED_APPS = [
